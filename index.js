0687651ba6e1676a8997d825e0e13179e1f3d9a5
Ian Sinnott <ian@iansinnott.com>
2017-01-30 22:21:46 -0800

    Update name to instyll

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-module.exports = function instyl(opts) {
+module.exports = function instyll(opts) {
   const implicit = (opts && opts.implicit == false) ? false : true;
 
   return function(style) {
     style.include(__dirname);
     if (implicit) {
-      style.import('instyl');
+      style.import('instyll');
     }
   };
 };
